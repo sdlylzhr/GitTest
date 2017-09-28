6c999a268b3af839d8da9649900ecb792c778202
dllo <783647393@qq.com>
2017-09-28 17:04:23 +0800

    再添加一个别人的

--- src/main/java/com/lanou/test/MainTest.java
+++ src/main/java/com/lanou/test/MainTest.java
@@ -4,6 +4,7 @@ package com.lanou.test;
  * Created by lizhongren1 on 2017/9/28.
  */
 public class MainTest {
+
     public static void main(String[] args) {
         System.out.println("hahaha");
 
@@ -15,6 +16,8 @@ public class MainTest {
         System.out.println("别睡觉, 会死!");
 
         Integer b = 3333;
+
+        System.out.println("厉害了,我的哥");
 
 
     }
