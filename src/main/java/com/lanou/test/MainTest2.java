1ee49c4613b95b01abffda80fda52138a2fefb04
dllo <1084372318@qq.com>
2017-09-28 17:04:55 +0800

    随便改一下

---
+++ src/main/java/com/lanou/test/MainTest2.java
@@ -0,0 +1,14 @@
+package com.lanou.test;
+
+/**
+ * Created by dllo on 17/9/28.
+ */
+public class MainTest2 {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("新建一个文件，啦啦啦");
+    }
+
+}
